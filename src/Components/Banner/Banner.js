cfc56548d1e8a16778422756063430bb2936b36c
Rayhan-dev <mohammodrayhan444@gmail.com>
2022-04-18 23:29:06 +0600

    banner text edited

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -6,6 +6,7 @@ const Banner = () => {
         <div className='container banner py-5'>
             <div className="row ">
                 <div className="col-md-5 my-auto">
+                    <h1>Hey <span className='text-primary'>Rayhan</span> here</h1>
                     <h1 className='text-primary text-start'>Best Tutor Focused on your Success</h1>
                 </div>
                 <div className="col-md-7 my-auto">
