5cac38000ed62e9931efefada48f2870a1564ac1
Rayhan-dev <mohammodrayhan444@gmail.com>
2022-04-18 23:17:32 +0600

    checkout page edited

--- src/Components/ServiceCheckOut/ServiceCheckOut.js
+++ src/Components/ServiceCheckOut/ServiceCheckOut.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { Button} from "react-bootstrap";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const ServiceCheckOut = () => {
     const { serviceId } = useParams();
     return (
-    <div className="container">
+    <div style={{minHeight:'75vh'}} className="container">
       <h1>Selected product</h1>
         <h2>this is checkout root for product : {serviceId}</h2>
-        <Link to={"/checkout"}><Button className="btn btn-primary">Proceed to Checkout</Button></Link>
     </div>
   );
 };
